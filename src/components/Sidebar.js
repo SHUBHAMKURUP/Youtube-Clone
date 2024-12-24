2b49586e93ab352519ac48dd49ef7fdbe8b36e42
shubham <shubham3325@gmail.com>
2024-12-24 12:23:44 +0530

    Updated Sidebar component and package dependencies

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,31 +3,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { GoHome } from "react-icons/go";
-import {
-  MdOutlineSubscriptions,
-  MdHistory,
-  MdOutlinedFlag,
-} from "react-icons/md";
-import { PiUserSquareThin } from "react-icons/pi";
+import { MdOutlineSubscriptions, MdHistory } from "react-icons/md";
+
 import { IoGameControllerOutline } from "react-icons/io5";
 import { AiOutlineLike } from "react-icons/ai";
 import { FaChevronRight } from "react-icons/fa6";
-import { FaYoutube } from "react-icons/fa";
 import { SiYoutubestudio } from "react-icons/si";
 import { SiYoutubekids } from "react-icons/si";
 import { MdOutlineWatchLater } from "react-icons/md";
 import { SiYoutubemusic } from "react-icons/si";
-import { SiTrendmicro } from "react-icons/si";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { PiFilmSlateLight } from "react-icons/pi";
 import { CgMediaLive } from "react-icons/cg";
-import { SiYoutubegaming } from "react-icons/si";
-import { FaRegNewspaper } from "react-icons/fa";
 import { TfiCup } from "react-icons/tfi";
 import { PiLightbulbLight } from "react-icons/pi";
 import { SiStylelint } from "react-icons/si";
 import { MdPodcasts } from "react-icons/md";
-import { BiNews, BiShoppingBag, BiVideo } from "react-icons/bi";
+import { BiNews, BiVideo } from "react-icons/bi";
 import { GiLinkedRings } from "react-icons/gi";
 import { LiaDownloadSolid } from "react-icons/lia";
 import { LiaFireAltSolid } from "react-icons/lia";
@@ -90,7 +82,7 @@ const Sidebar = () => {
     {
       id: 6,
       name: "Liked Videos",
-      icon: <GiLinkedRings />,
+      icon: <AiOutlineLike />,
     },
     {
       id: 7,
